aab58552f4b2ad37d04bafd65aa5f72f06eb005e
August J. Lee <77298066+augustlee2000@users.noreply.github.com>
2023-06-22 11:29:30 -0500

    Update square wave.py

--- square wave.py
+++ square wave.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import numpy as np
 from math import pi, sqrt, exp, cos, log
 import matplotlib.pyplot as plt
@@ -10,15 +9,17 @@ from scipy.stats import moyal
 from matplotlib.widgets import Slider, Button, TextBox
 %matplotlib widget
 
-
+#function that give back an area that represent a square wave 
 def squarewave(time_array, frequency, phase):
     squarewave_temp = signal.square(2*pi*frequency*time_array + phase)
     squarewave_array = squarewave_mover(squarewave_temp)
     return squarewave_array
 
+#a simple form of the PDF of the landau distrubtuion 
 def landau(time_array):
     return ((1/sqrt(2*pi)) * exp(-1 * (time_array  + exp(-1*time_array))/2) *4)
 
+#The orginal square wave is from -1 to 1 I wanted it from 0 to 1
 def squarewave_mover(squarewave):
     for i in range(len(squarewave)):
         if squarewave[i] < 0:
@@ -27,16 +28,11 @@ def squarewave_mover(squarewave):
             continue
     return squarewave
 
-#def landau_array_creator(time_array, landau_x_shift):
-    #landau_array = np.array([])
-    #for i in range(len(time_array)):
-        #landau_array = np.append(landau_array, landau(time_array[i] -landau_x_shift))
-    #return landau_array
-
-
+#the PDF of the landau distribution used in the integration
 def integrand(t):
     return (np.exp(-t) * np.cos(t*((x-u)/c) + (2*t*np.log(t/c))/(pi)))
 
+#This create the array for the landau distrubution 
 def landau_array_creator(time_array):
     global x
     test = np.array([])
@@ -51,7 +47,7 @@ def landau_array_creator(time_array):
 
     
 
-
+#a quick and easy rectangluar integration calculator for the landau distrubution where it only does the integral if the square wave is 1
 def integral_calculator(time_array, landau_array, timer_array, spacing):
     integral = 0.0
     for i in range(len(time_array)):
@@ -61,12 +57,14 @@ def integral_calculator(time_array, landau_array, timer_array, spacing):
             continue
     return integral
 
+#does the full integral
 def integral_calculator_super_lazy(time_array, landau_array, spaceing):
     integral = 0.0
     for i in range(len(time_array)):
         integral += landau_array[i] * spacing
     return integral
-            
+
+# makes an array that shows the threshold         
 def threshold_calc(time_array, threshold):
     temp = np.array([])
     for i in range(len(time_array)):
@@ -93,19 +91,21 @@ def update(val):
     t1.set_text("The Total Percentage of Electrons seen by the Trigger: " + str(round(integral_calculator(time_array,x1, y1, spacing),4)))
     fig.canvas.draw_idle()
 
+# Resets the sliders
 def reset(event):
     phase_slider.reset()
     pos_slider.reset()
     thresh_slider.reset()
 
 
-    
+#making the time array     
 N = 500
 time_end = 150
 time_array = np.linspace(0, time_end, N)
 
 spacing = time_end/N
 
+#all of our inital conditions
 frequency_timer = .025
 frequency_tdc = frequency_timer * 8
 phase_timer = 0
@@ -118,21 +118,20 @@ c = 10
 u = 5
 x = 0.0
 
+#makes the timer and tdc squarewaves
 squarewave_timer = squarewave(time_array,frequency_timer, phase_timer)
 squarewave_tdc = squarewave(time_array,frequency_tdc,phase_timer)
 
 
-#landau_array = landau_array_creator(time_array, landau_x_shift)
+#makes the landau distrubutino
 landau_array = landau_array_creator(time_array)
 
-
-
-                   
+#find the integral of the landau distrubution and normalized
 integral_full = integral_calculator_super_lazy(time_array, landau_array, spacing)
                    
 landau_array  = landau_array/integral_full
 
-
+#makes the threshhold
 threshold_array = threshold_calc(time_array, threshold_int)
 
 # Create the figure and subplots
@@ -148,7 +147,7 @@ fig.legend()
 
 fig.subplots_adjust(left=.20, bottom=0.22)
 
-
+#makes the three slider that we want in the program
 axfreq = fig.add_axes([0.25, 0.05, 0.65, 0.03])
 phase_slider = Slider(
     ax=axfreq,
@@ -179,7 +178,8 @@ thresh_slider = Slider(
 
 
 t1 = fig.text(.05, .90,  "The Total Percentage of Electrons seen by the Trigger: " +str(round(integral_calculator(time_array,landau_array, squarewave_timer, spacing),4)))
-   
+
+#updates the sliders
 phase_slider.on_changed(update)
 pos_slider.on_changed(update)
 thresh_slider.on_changed(update)
